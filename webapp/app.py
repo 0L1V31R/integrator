c723626841da3759955a8b21108070a89cd47aa2
0L1V31R <eng.samuel@ymail.com>
2021-11-22 13:06:15 -0300

    added background, logo senai and background-color for temperature

--- webapp/app.py
+++ webapp/app.py
@@ -5,21 +5,34 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     aluno = '/static/images/user-regular.svg'
-    return render_template('index.html', aluno=aluno)
+    senai = '/static/images/senai.png'
+    template = {
+        'aluno' : aluno,
+        'senai': senai,
+    }
+    return render_template('index.html', **template)
 
 @app.route('/<card>&<temps>')
 def index2(card=0, temps=0):
     aluno = '/static/images/user-regular.svg'
+    senai = '/static/images/senai.png'
     data = {
         '0': 'Avaster',
         '1': 'Alumnster'
     }
     nome_do_aluno = data[card]
+    if(float(temps) <= 37.5):
+        tipo = 'normal'
+    else:
+        tipo = 'nonormal'
+    
     template = {
         'cartao': card,
         'temperatura': temps,
         'aluno': aluno,
         'nome': nome_do_aluno,
+        'tipo': tipo,
+        'senai': senai,
     }
     template['redirect_url'] = url_for('index')
     return render_template('index2.html', **template)
